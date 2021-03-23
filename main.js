b4e743c0759711b1b16da510b6d37f6a6d266878
Kheer-Thakkar <77600784+Kheer-Thakkar@users.noreply.github.com>
2021-03-23 23:45:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var canvas=new fabric.Canvas("myCanvas");
  var block_image_object="";
 
  function player_update(){
-    fabric.Image.fromURL("https://drive.google.com/file/d/1-hMfzjM02r_h02NDgkQk9WmFTB-ZErBt/view?usp=sharing",function(Img){
+    fabric.Image.fromURL("https://i.postimg.cc/zDwfFdYY/player.png",function(Img){
         player_object=Img;
         player_object.scaleToWidth(150);
         player_object.scaleToHeight(140);
@@ -36,32 +36,57 @@ function new_image(get_image){
 
 });
 }
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+if(e.shiftKey == true && keyPressed == '80')
+{
+	console.log("p and shift pressed together");
+	block_image_width = block_image_width + 10;
+	block_image_height = block_image_height + 10;
+	document.getElementById("current_width").innerHTML = block_image_width;
+	document.getElementById("current_height").innerHTML = block_image_height;	
+}
+if(e.shiftKey && keyPressed == '77')
+{
+	console.log("m and shift pressed together");
+	block_image_width = block_image_width - 10;
+	block_image_height = block_image_height - 10;
+	document.getElementById("current_width").innerHTML = block_image_width;
+	document.getElementById("current_height").innerHTML = block_image_height;
+}
+
+
 if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/hGnyTPLB/ironman-face.png");
+    new_image("ironman-face.png");
     console.log("f");
     
  }
 
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/FscwL6M0/spiderman-body.png");
+ if (keyPressed == '66'){
+    new_image("spiderman-body.png");
     console.log("b");
     
  }
 
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/KzF6GDqt/hulk-legs.png");
+ if (keyPressed == '76'){
+    new_image("hulk-legs.png");
     console.log("l");
     
  }
 
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/8s8BGtwS/thor-right-hand.png");
+ if (keyPressed == '82'){
+    new_image("thor-right-hand.png");
     console.log("r");
     
  }
 
- if (keyPressed == '70'){
-    new_image("https://i.postimg.cc/rw7ckW29/captain-america-left-hand.png");
+ if (keyPressed == '72'){
+    new_image("captain-america-left-hand.png");
     console.log("h");
     
  }
@@ -85,4 +110,48 @@ if (keyPressed == '39'){
    right();
    console.log("right");
    
+}}
+
+function up()
+{
+	if(player_y >=0)
+	{
+		player_y = player_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
 }
+function down(){
+if(player_y <=500)
+	{
+		player_y = player_y - block_image_height;
+		console.log("block image height = " + block_image_height);
+		console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+		canvas.remove(player_object);
+		player_update();
+	}
+}
+
+function left(){
+   if(player_x >=0)
+      {
+         player_x = player_x - block_image_height;
+         console.log("block image height = " + block_image_height);
+         console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+         canvas.remove(player_object);
+         player_update();
+      }
+   }
+
+   function right(){
+      if(player_x <=850)
+         {
+            player_x = player_x + block_image_height;
+            console.log("block image height = " + block_image_height);
+            console.log("When Up arrow key is pressed, X =  " + player_x + " , Y = "+player_y);
+            canvas.remove(player_object);
+            player_update();
+         }
+   }
